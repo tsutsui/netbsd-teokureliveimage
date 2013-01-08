21dbcb16cc7c3fa7b9a7ed2d7679d20571ace84e
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2013-01-08 23:11:50 +0900

    Remove extra absolute path.

--- prepare-images.sh
+++ prepare-images.sh
@@ -13,5 +13,5 @@ dd if=work.amd64.usb/liveimage-amd64-usb-${REVISION}.img count=${USBMB} bs=1m \
 gzip -9c work.i386.emu/liveimage-i386-emu-${REVISION}.img \
     > ${RELDIR}/liveimage-i386-emu-${REVISION}.img.gz
 
-gzip -9c /home/tsutsui/teokure/setupliveimage-${REVISION}.fs \
+gzip -9c ./setupliveimage-${REVISION}.fs \
     > ${RELDIR}/setupliveimage-${REVISION}.fs.gz
