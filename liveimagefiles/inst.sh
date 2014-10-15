67c3949ec0834760ca118c80fa895dd1e1e82919
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2014-10-15 21:58:37 +0900

    Add and install /etc/fonts/local.conf for updated pkgsrc X11 fontpath.

--- liveimagefiles/inst.sh
+++ liveimagefiles/inst.sh
@@ -72,6 +72,9 @@ echo hal=YES				>> /etc/rc.conf
 echo avahidaemon=NO			>> /etc/rc.conf
 echo nasd=NO				>> /etc/rc.conf
 
+# copy local fontconfig settings for pkgsrc fonts
+cp ${FILEDIR}/etc/fonts/local.conf /etc/fonts
+
 # copy files for asound.conf
 cp ${FILEDIR}/etc/asound.conf /etc
 
