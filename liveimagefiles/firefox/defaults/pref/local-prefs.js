79985f1950b27b7687760bba3420de8f38ef6fb7
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2018-03-18 09:36:39 +0900

    Add "intl.local.requested" pref to enable locale by LANG. Found on ubuntu.

--- liveimagefiles/firefox/defaults/pref/local-prefs.js
+++ liveimagefiles/firefox/defaults/pref/local-prefs.js
@@ -1,5 +1,6 @@
 // choose useragent locale per LANG
 pref("intl.locale.matchOS", true);
+pref("intl.locale.requested", "");
 
 // Enalbe Network Manager integration
 pref("network.manage-offline-status", true);
