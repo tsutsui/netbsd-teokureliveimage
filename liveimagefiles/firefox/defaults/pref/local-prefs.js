3146c37c1e9f392495cc444c047d5629fdad6881
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2018-03-18 09:33:34 +0900

    Sync with ubuntu's vendor-gre.js settings.

--- liveimagefiles/firefox/defaults/pref/local-prefs.js
+++ liveimagefiles/firefox/defaults/pref/local-prefs.js
@@ -1,5 +1,5 @@
 // choose useragent locale per LANG
 pref("intl.locale.matchOS", true);
 
-// use default en-US on searchplugin regardless of LANG
-pref("distribution.searchplugins.defaultLocale", "en-US");
+// Enalbe Network Manager integration
+pref("network.manage-offline-status", true);
