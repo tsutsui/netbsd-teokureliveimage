d25dcac9e64eebbea9cdb61302016d4475cfecac
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2019-01-02 23:33:04 +0900

    Add hunspell settings for firefox.
    Taken from the latest ubuntu.
     https://bazaar.launchpad.net/~mozillateam/firefox/firefox.bionic/view/head:/debian/vendor-gre.js

--- liveimagefiles/firefox/defaults/pref/local-prefs.js
+++ liveimagefiles/firefox/defaults/pref/local-prefs.js
@@ -3,3 +3,6 @@ pref("intl.locale.requested", "");
 
 // Enalbe Network Manager integration
 pref("network.manage-offline-status", true);
+
+// load system dictionaries
+pref("spellchecker.dictionary_path", "/usr/pkg/share/hunspell");
