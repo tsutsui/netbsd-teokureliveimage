a015d7c03a09f87506b57cd52d15b7a069e8ac3c
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2019-01-03 06:48:06 +0900

    Fix typo etc.

--- liveimagefiles/firefox/defaults/pref/local-prefs.js
+++ liveimagefiles/firefox/defaults/pref/local-prefs.js
@@ -1,10 +1,10 @@
-// choose useragent locale per LANG
+// Choose user agent locale per LANG
 pref("intl.locale.requested", "");
 
 // Assume prefered languages to show pages per LANG
 pref("intl_accept_languages", "");
 
-// Enalbe Network Manager integration
+// Enable Network Manager integration
 pref("network.manage-offline-status", true);
 
 // load system dictionaries
