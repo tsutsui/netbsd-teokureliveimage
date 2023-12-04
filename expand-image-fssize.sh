a36cde9af05a96a3268896f32aaefaa17e63437f
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2023-12-05 02:22:56 +0900

    Add support to expand an FFS partition in the UEFI version image. #12

--- expand-image-fssize.sh
+++ expand-image-fssize.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# Copyright (c) 2013 Izumi Tsutsui.  All rights reserved.
+# Copyright (c) 2013, 2023 Izumi Tsutsui.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -28,14 +28,28 @@ echo Start expanding fs size upto the actual disk size...
 
 # make sure we are on ${BOOTDISK} on root
 ROOTDEV=$(sysctl -n kern.root_device)
-if [ "${ROOTDEV}"X != ${BOOTDISK}X ] ; then
-	echo Error: root file system device is not ${BOOTDISK}
+
+case "${ROOTDEV}" in
+(dk[0-9]*)
+	USE_GPT=yes
+	BLOCKDEV=$(dkctl ${ROOTDEV} getwedgeinfo | head -1 | sed 's/://' | awk '{print $3}')
+	ROOTPART=${ROOTDEV}
+	;;
+(*)
+	USE_GPT=no
+	BLOCKDEV=${ROOTDEV}
+	ROOTPART=${ROOTDEV}a
+	;;
+esac
+
+if [ "${BLOCKDEV}"X != "${BOOTDISK}"X ] ; then
+	echo "Error: root file system device (${BLOCKDEV}) is not ${BOOTDISK}"
 	exit 1
 fi
 
 # make sure target disk is not mounted
-if (mount | grep -q ^/dev/${BOOTDISK}a) ; then
-	echo Error: /dev/${BOOTDISK}a is already mounted
+if (mount | grep -q ^/dev/${ROOTPART}) ; then
+	echo Error: /dev/${ROOTPART} is already mounted
 	exit 1
 fi
 
@@ -50,9 +64,63 @@ IMAGESECTORS=$(echo ${BOOTDISKDMSG} | awk '{print $(NF-1)}')
 
 ORIGIMAGEMB=5120
 ORIGSWAPMB=512
+ORIGGPTMB=1
 
 ORIGIMAGESECTORS=$((${ORIGIMAGEMB} * 1024 * 1024 / 512))
 ORIGSWAPSECTORS=$((${ORIGSWAPMB} * 1024 * 1024 / 512))
+ORIGGPTSECTORS=$((${ORIGGPTMB} * 1024 * 1024 / 512))
+
+if [ "${USE_GPT}" = "yes" ]; then
+
+GPTROOTLABEL=TeokureLiveImage_root
+GPTSWAPLABEL=TeokureLiveImage_swap
+
+# check label names
+ROOTLABEL=$(gpt show -i 2 ${BLOCKDEV} | grep ^Label: | sed -e 's/Label: //g')
+SWAPLABEL=$(gpt show -i 3 ${BLOCKDEV} | grep ^Label: | sed -e 's/Label: //g')
+if [ "${ROOTLABEL}"X != "${GPTROOTLABEL}"X ]; then
+	echo Error: unexpected root label : \"${ROOTLABEL}\"
+	exit 1
+fi
+if [ "${SWAPLABEL}"X != "${GPTSWAPLABEL}"X ]; then
+	echo Error: unexpected swap label : \"${SWAPLABEL}\"
+	exit 1
+fi
+
+echo ${GPTROOTLABEL} found in ${BOOTDISK} GPT partition.
+
+LASTSECTORS=$(($(gpt show ${BLOCKDEV} | grep 'Sec GPT header' | awk '{print $1}') + 1))
+
+if [ ${LASTSECTORS} -ne ${ORIGIMAGESECTORS} ]; then
+	echo Error: unexpected image size in GPT partition : ${LASTSECTORS}
+	echo Expected original image size: ${ORIGIMAGESECTORS}
+	exit 1
+fi
+
+echo Original image size: ${ORIGIMAGESECTORS} sectors
+echo Target ${BLOCKDEV} disk size: ${IMAGESECTORS} sectors
+
+if [ ${ORIGIMAGESECTORS} -gt ${IMAGESECTORS} ]; then
+	echo Error: ${BLOCKDEV} is too small?
+	exit 1
+fi
+
+# calculate new disk parameters
+SWAPSECTORS=${ORIGSWAPSECTORS}
+SWAPOFFSET=$((${IMAGESECTORS} - ${ORIGGPTSECTORS} - ${ORIGSWAPSECTORS}))
+
+# check original fs
+echo Checking file system...
+fsck_ffs -p /dev/r${ROOTPART}
+
+# update GPT partitions
+gpt resizedisk ${BLOCKDEV}
+gpt remove -i 3 ${BLOCKDEV}		# remove original swap partitoin
+gpt add -a 1m -b ${SWAPOFFSET} -s ${SWAPSECTORS} -i 3 \
+    -t swap -l ${SWAPLABEL} ${BLOCKDEV}	# add new swap partition at the end
+gpt resize -i 2 ${BLOCKDEV}		# expand FFS partition
+
+else # "${USE_GPT}" = "no"
 
 # mount tmpfs to create work file
 if ! (mount | grep -q '^tmpfs on /tmp') ; then
@@ -129,7 +197,7 @@ sed -e "s/^cylinders: [0-9]*$/cylinders: ${CYLINDERS}/" \
 
 # check original fs
 echo Checking file system...
-fsck_ffs -p /dev/r${BOOTDISK}a
+fsck_ffs -p /dev/r${ROOTPART}
 
 # update MBR label
 echo Updating partition size in MBR label...
@@ -141,9 +209,11 @@ fdisk -f -u -b ${MBRCYLINDERS}/${BHEAD}/${BSEC} \
 echo Updating partition size in disklabel...
 disklabel -R ${BOOTDISK} /tmp/disklabel.${BOOTDISK}.new
 
+fi # "${USE_GPT}" = "yes"
+
 # update fs size
 echo Perform resize_ffs...
-resize_ffs -p -y /dev/r${BOOTDISK}a
+resize_ffs -p -y /dev/r${ROOTPART}
 echo Done!
 echo
 echo Hit Enter to reboot...
