fb763e9697bd96d5b2f29e58c744d0b843ba945b
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2024-04-18 02:32:44 +0900

    Update scripts for NetBSD 10.0 release.

--- mkliveimage.sh
+++ mkliveimage.sh
@@ -149,7 +149,7 @@ fi
 #FTPHOST=ftp7.jp.NetBSD.org
 FTPHOST=cdn.NetBSD.org
 #FTPHOST=nycdn.NetBSD.org
-RELEASE=10.0_RC2
+RELEASE=10.0
 RELEASEDIR=pub/NetBSD/NetBSD-${RELEASE}
 #RELEASEDIR=pub/NetBSD-daily/netbsd-10/latest
 
